4229b73f1a3aec77a67555573b6ea024e08057a0
GokulP <pgokul695@gmail.com>
2026-07-19 02:13:11 +0530

    chore: allow proxied Vite host

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    port: 5015,
+    strictPort: true,
+    allowedHosts: ['schedge.gokulp.online'],
+  },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
